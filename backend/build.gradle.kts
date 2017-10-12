7dafb91b0834e6775ee8d02c3891800f764cf6be
Sebastien Deleuze <sdeleuze@pivotal.io>
2017-10-12 10:12:54 +0200

    Upgrade to Spring Boot 2.0.0.M5

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -3,31 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.jetbrains.kotlin.jvm") version "1.1.51"
 	id("org.jetbrains.kotlin.plugin.spring") version "1.1.51"
+    id("org.springframework.boot") version "2.0.0.M5"
 	id("io.spring.dependency-management") version "1.0.3.RELEASE"
-}
-
-buildscript {
-	repositories {
-		mavenCentral()
-		jcenter()
-		maven("https://repo.spring.io/milestone")
-		maven("https://repo.spring.io/snapshot")
-	}
-
-	dependencies {
-		classpath("org.springframework.boot:spring-boot-gradle-plugin:2.0.0.BUILD-SNAPSHOT")
-	}
-}
-
-apply {
-	plugin("org.springframework.boot")
-
 }
 
 repositories {
 	mavenCentral()
 	maven("https://repo.spring.io/milestone")
-	maven("https://repo.spring.io/snapshot")
 }
 
 tasks {
